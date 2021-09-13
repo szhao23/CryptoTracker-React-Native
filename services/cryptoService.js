9cf89def14f86e36a845778eb35213ac3a0926d7
szhao23 <stevenzhao456@gmai.com>
2021-09-13 15:10:52 -0500

    Finalized App for Usage

---
+++ services/cryptoService.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+import moment from "moment";
+
+const formatSparkline = (numbers) => {
+  const sevenDaysAgo = moment().subtract(7, "days").unix();
+
+  let formattedSparkline = numbers.map((item, index) => {
+    return {
+      x: sevenDaysAgo + (index + 1) * 3600,
+      y: item,
+    };
+  });
+
+  return formattedSparkline;
+};
+
+const formatMarketData = (data) => {
+  let formattedData = [];
+
+  data.forEach((item) => {
+    const formattedSparkline = formatSparkline(item.sparkline_in_7d.price);
+
+    const formattedItem = {
+      ...item,
+      sparkline_in_7d: {
+        price: formattedSparkline,
+      },
+    };
+
+    formattedData.push(formattedItem);
+  });
+
+  return formattedData;
+};
+
+export const getMarketData = async () => {
+  try {
+    // this is where the API call will go
+    const response = await axios.get(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=true&price_change_percentage=7d"
+    );
+
+    const data = response.data;
+    const formattedResponse = formatMarketData(data);
+
+    return formattedResponse;
+  } catch (err) {
+    console.log(err);
+  }
+};
